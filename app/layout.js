e01200b75b0d495071183645b018f6f458db550c
Thomas Basham <Bashamtg@gmail.com>
2026-03-09 16:29:05 -0700

    added google tag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import Providers from "./providers";
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export const metadata = {
   title: "Collab Done",
@@ -17,6 +20,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {GA_MEASUREMENT_ID ? (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                window.gtag = gtag;
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        ) : null}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
